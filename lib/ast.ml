4d06eda3631c71896721296f3a94493b0c6e8243
Yubing Zhang <joseph.zyb@gmail.com>
2026-05-22 10:33:10 +0800

    feat: define ast of cv

---
+++ lib/ast.ml
@@ -0,0 +1,33 @@
+type text = string
+
+type contact =
+  | Plain of string
+  | Hyperlink of string * string
+
+type headline = {
+  name : string;
+  labels : contact list
+}
+
+type heading = {
+  title : string;
+  subtitle : string;
+  location : string;
+  start_date : string;
+  end_date : string option;
+}
+
+type item = {
+  heading : heading option;
+  bullets : text list;
+}
+
+type section = {
+  title : string;
+  items : item list;
+}
+
+type cv = {
+  headline : headline;
+  sections : section list;
+}
